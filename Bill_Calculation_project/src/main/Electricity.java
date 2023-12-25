413c9b7be7665ca031e229954e929d342d12826b
Sadia <sp22bsse0067@maju.edu.pk>
2023-12-25 22:08:49 +0500

    web service

--- Bill_Calculation_project/src/main/Electricity.java
+++ Bill_Calculation_project/src/main/Electricity.java
@@ -9,14 +9,14 @@ public class Electricity {
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con =
-			DriverManager.getConnection("jdbc:mysql://localhost:3306/tuturial","root","");
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/add_new_meter","root","");
 			Statement stmt=con.createStatement();
-			ResultSet rs=stmt.executeQuery("select * from teacher");
+			ResultSet rs=stmt.executeQuery("select * from admin");
 
 			while(rs.next())
 			{
-			System.out.println("ID: " + rs.getInt(1) + " Name: " + rs.getString(2) + " Designation: "
-			+ rs.getString(3) +  " roomid: " + rs.getInt(4));
+			System.out.println("Admin_id: " + rs.getInt(1) + " username: " + rs.getString(2) + " email: "
+			+ rs.getString(3) +  " password: " + rs.getString(4));
 			}
 			con.close();
 			} catch(Exception e)
