d25e8235e95eeadd15bfc0645d81ff2870051621
Sadia <sp22bsse0067@maju.edu.pk>
2023-12-27 15:16:31 +0500

    fixed sql to one

--- add_new_meter.sql
+++ add_new_meter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2023 at 11:13 AM
+-- Generation Time: Dec 27, 2023 at 11:15 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -47,6 +47,29 @@ INSERT INTO `admin` (`Admin_id`, `Username`, `Password`, `email`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `consumption_data`
+--
+
+CREATE TABLE `consumption_data` (
+  `Data_id` int(11) NOT NULL,
+  `Meter_No` int(11) NOT NULL,
+  `Month_Year` varchar(45) NOT NULL,
+  `Unit Consumption` int(11) NOT NULL,
+  `ToTal_Amount` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `consumption_data`
+--
+
+INSERT INTO `consumption_data` (`Data_id`, `Meter_No`, `Month_Year`, `Unit Consumption`, `ToTal_Amount`) VALUES
+(1, 23456, 'January-2023', 300, NULL),
+(2, 34567, 'March-2023', 280, NULL),
+(3, 45678, 'December-2023', 240, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customer`
 --
 
@@ -65,7 +88,8 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`customer_id`, `customer_name`, `adress`, `email`, `ph_no`) VALUES
 (1, 'Zaheer-ud-din', 'Nazmabad', 'zaheer234@gmail.com', '0315-675476'),
 (2, 'Anwar Jamel', 'Gulistan-e-johar', 'anwar786@gmail.com', '0345-286754'),
-(3, 'Zeeshan Ahmed', 'Shahra-e-Faisal', 'zeeshan67@gmail.com', '0325-5366357');
+(3, 'Zeeshan Ahmed', 'Shahra-e-Faisal', 'zeeshan67@gmail.com', '0325-5366357'),
+(6, 'sadia', 'nazimabad', 'sadia123', '034569078');
 
 -- --------------------------------------------------------
 
@@ -74,9 +98,10 @@ INSERT INTO `customer` (`customer_id`, `customer_name`, `adress`, `email`, `ph_n
 --
 
 CREATE TABLE `meter_data` (
-  `Meter_no` bigint(20) NOT NULL,
-  `location` varchar(45) NOT NULL,
-  `installation_date` date NOT NULL,
+  `Meter_no` int(11) NOT NULL,
+  `Customer_Id` int(11) NOT NULL,
+  `Location` varchar(45) NOT NULL,
+  `Installation_Date` date NOT NULL,
   `Status` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -84,36 +109,10 @@ CREATE TABLE `meter_data` (
 -- Dumping data for table `meter_data`
 --
 
-INSERT INTO `meter_data` (`Meter_no`, `location`, `installation_date`, `Status`) VALUES
-(23456, '24 main street', '2023-12-03', 'Active'),
-(34567, 'grounfloor, room1', '2023-11-05', 'active'),
-(45678, 'BuldingA ,basement', '2022-12-10', 'Active');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `meter_reading`
---
-
-CREATE TABLE `meter_reading` (
-  `Reading_date` date NOT NULL,
-  `Readig_value/units` varchar(45) NOT NULL,
-  `Meter_no` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `meter_reading`
---
-
-INSERT INTO `meter_reading` (`Reading_date`, `Readig_value/units`, `Meter_no`) VALUES
-('0000-00-00', '450', 23456),
-('2023-02-01', '365 ', 34567),
-('0000-00-00', '430', 34567),
-('2023-04-01', '450', 23456),
-('2023-02-01', '365 ', 34567),
-('0000-00-00', '430', 45678),
-('2023-12-01', '98', 45678),
-('2023-06-01', '650', 23456);
+INSERT INTO `meter_data` (`Meter_no`, `Customer_Id`, `Location`, `Installation_Date`, `Status`) VALUES
+(23456, 1, '24 main street', '2023-12-03', 'Active'),
+(34567, 3, 'ground floor,room1', '2023-11-05', 'Active'),
+(45678, 2, 'Building A, Basement', '2022-12-10', 'Active');
 
 --
 -- Indexes for dumped tables
@@ -126,6 +125,13 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`Admin_id`);
 
 --
+-- Indexes for table `consumption_data`
+--
+ALTER TABLE `consumption_data`
+  ADD PRIMARY KEY (`Data_id`),
+  ADD KEY `Meter_No` (`Meter_No`);
+
+--
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
@@ -135,13 +141,24 @@ ALTER TABLE `customer`
 -- Indexes for table `meter_data`
 --
 ALTER TABLE `meter_data`
-  ADD PRIMARY KEY (`Meter_no`);
+  ADD PRIMARY KEY (`Meter_no`),
+  ADD KEY `Customer_id` (`Customer_Id`);
 
 --
--- Indexes for table `meter_reading`
+-- Constraints for dumped tables
 --
-ALTER TABLE `meter_reading`
-  ADD KEY `meter_no_fk` (`Meter_no`);
+
+--
+-- Constraints for table `consumption_data`
+--
+ALTER TABLE `consumption_data`
+  ADD CONSTRAINT `Meter_No` FOREIGN KEY (`Meter_No`) REFERENCES `meter_data` (`Meter_no`);
+
+--
+-- Constraints for table `meter_data`
+--
+ALTER TABLE `meter_data`
+  ADD CONSTRAINT `Customer_id` FOREIGN KEY (`Customer_Id`) REFERENCES `customer` (`customer_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
